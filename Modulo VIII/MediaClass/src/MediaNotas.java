1e4044a8c4007cf59a09fa9e22fad234b6ada021
Bruna <brunadalmagrodev@gmail.com>
2023-08-10 13:55:40 -0300

    Media Class

---
+++ Modulo VIII/MediaClass/src/MediaNotas.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; // importando o método Scanner para leitura das notas
+
+public class MediaNotas{
+    public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        System.out.println("Digite a quarta nota: ");
+        double nota4 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
