b2bcdd30ba79c6b82f6efc07caab2cf68ab0a5b6
Bruna <brunadalmagrodev@gmail.com>
2023-08-11 10:31:35 -0300

    Tipos compolexos

---
+++ Modulo VX/Tipos Complexos/src/ConversaoWrapper.java
@@ -0,0 +1,7 @@
+public class ConversaoWrapper {
+    public static void main(String[] args) {
+        int valorPrimitivo = 855000000;
+        Short valorWrapper = (short) valorPrimitivo;
+        System.out.println(valorPrimitivo);
+    }
+}
